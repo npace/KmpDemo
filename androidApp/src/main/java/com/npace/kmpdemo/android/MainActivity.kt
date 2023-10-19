360bda615f31e98a339ad0b3cf67353d104ab36a
Lubomir Naydenov <lubonay@gmail.com>
2023-10-19 08:11:20 +0300

    Implement Android native UI with Jetpack Compose

--- androidApp/src/main/java/com/npace/kmpdemo/android/MainActivity.kt
+++ androidApp/src/main/java/com/npace/kmpdemo/android/MainActivity.kt
@@ -3,12 +3,24 @@ package com.npace.kmpdemo.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.cardElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.npace.kmpdemo.Greeting
+import androidx.compose.ui.unit.dp
+import com.npace.kmpdemo.CheeseViewStateFakeData
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    CheeseList()
                 }
             }
         }
@@ -27,14 +39,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
+private fun CheeseList() {
+    val items = remember {
+        CheeseViewStateFakeData.cheeseViewStates
     }
+    LazyColumn(
+        modifier = Modifier.padding(8.dp),
+        content = {
+            items(
+                items = items,
+                key = { it.cheeseName },
+                itemContent = {
+                    Card(
+                        elevation = cardElevation(
+                            defaultElevation = 8.dp,
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(8.dp),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Text(it.cheeseName)
+                        }
+                    }
+                }
+            )
+        }
+    )
 }

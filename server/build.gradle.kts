ddcdf5863e2e5979258fc7067ef686de92b2ca13
Lubomir Naydenov <lubonay@gmail.com>
2023-10-22 14:40:58 +0300

    Add Ktor dependency
    Steps:
      1. Add Ktor and Logback libraries + Ktor plugin
      2. Use the new dependencies in the server module
      3. Set the server's Java compatibility to version 17

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,9 +2,22 @@
 plugins {
     id("java-library")
     alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.ktor)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.ktor.server.core.jvm)
+    implementation(libs.ktor.server.netty.jvm)
+    implementation(libs.ktor.server.status.pages.jvm)
+    implementation(libs.ktor.server.default.headers.jvm)
+    implementation(libs.logback)
 }

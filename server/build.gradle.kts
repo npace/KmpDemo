00aa706b78c4b3ebcf358707a37e316f4f221dc5
Lubomir Naydenov <lubonay@gmail.com>
2023-10-22 14:55:51 +0300

    Clean up server build file

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,7 +1,6 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     id("java-library")
-    alias(libs.plugins.kotlinJvm)
+    kotlin("jvm")
     alias(libs.plugins.ktor)
 }
 

08969f55999c31c771b9e6800e980272d7e6bad7
Lubomir Naydenov <lubonay@gmail.com>
2023-10-22 18:28:34 +0300

    Add server URL host to Platform
    - The server is supposed to run locally on the development machine
    and be accessed by an Android emulator and an iOS simulator
    - Android emulators use 10.0.2.2 as the host machine's address

--- shared/src/androidMain/kotlin/com/npace/kmpdemo/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/npace/kmpdemo/Platform.android.kt
@@ -2,6 +2,7 @@ package com.npace.kmpdemo
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+    override val localServerHost: String = "http://10.0.2.2:8080"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/iosMain/kotlin/com/npace/kmpdemo/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/npace/kmpdemo/Platform.ios.kt
@@ -4,6 +4,7 @@ import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    override val localServerHost: String = "http://localhost:8080"
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()

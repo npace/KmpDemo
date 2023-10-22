c399e4dbc62106eff4ebb72781f20466eff96c50
Lubomir Naydenov <lubonay@gmail.com>
2023-10-22 15:26:59 +0300

    Add KotlinX Serialization plugin to :api

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.kotlinJvm) apply false
+    alias(libs.plugins.kotlinxSerialization) apply false
 }

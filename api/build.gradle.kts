a2bc3fa388359fcd9615662c7db70e83277b5625
Lubomir Naydenov <lubonay@gmail.com>
2023-10-22 15:12:39 +0300

    Add :api module for shared client/server models
    This time, the module was created manually:
      1. Create a new folder in the project, called "api"
      2. Add a basic KMP build.gradle.kts file in the new folder:
        - Apply the Kotlin Multiplatform plugin
        - Use the default target hierarchy with JVM and iOS support
        - Add a commonMain source set

---
+++ api/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
+kotlin {
+    targetHierarchy.default()
+
+    // for Android and Ktor JVM server
+    jvm()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "api"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting
+    }
+}
